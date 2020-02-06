a43c3c8fcc0e66fe101f1ac3377a9feff027f8dc
lbaitemple <lbai@temple.edu>
2020-02-06 09:32:15 -0500

    Update newtest2.sh

--- newtest2.sh
+++ newtest2.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 ###############
 # Please modify your cloud mqtt credential here
 ##############
+m_i2c="3c"
 m_server="m16.cloudmqtt.com"
 m_port=12247
-m_user="pspniyjc111"
-m_pass="sBm4EpaDgRe5111"
+m_user="pspniyjc1111"
+m_pass="sBm4EpaDgRe51111"
 m_topic="raspberry/ipaddress"
 
 ################
@@ -14,4 +15,15 @@ m_topic="raspberry/ipaddress"
 echo "Starting script sayIPbs "
 private=`hostname -I | sed -E -e 's/[[:blank:]]+/_/g' `
 res=`mosquitto_pub -h $m_server -p $m_port -u $m_user -P $m_pass -t $m_topic -m $private  > /dev/null 2>&1`
-#test=` python /home/ubuntu/stats.py`
+
+mapfile -t data < <(i2cdetect -y 1)
+
+for i in $(seq 1 ${#data[@]}); do
+    line=(${data[$i]})
+    echo ${line[@]:1} | grep -qw "$m_i2c"
+    if [ $? -eq 0 ]; then
+        test=` python /home/ubuntu/stats.py`
+        exit 0
+    fi
+done
+
